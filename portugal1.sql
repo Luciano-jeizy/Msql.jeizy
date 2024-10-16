d33c521c6a8aeccd6506c741e8188ced65688e2b
jeizy-luciano <164494003+Luciano-jeizy@users.noreply.github.com>
2024-10-16 16:28:28 +0100

    Add files via upload

---
+++ portugal1.sql
@@ -0,0 +1,20 @@
+InSERT INTO cidade(codigo_postal,nome_cidade) VALUES
+(1000,'Lisboa'),
+(4150,'Porto'),
+(4430,'Vila Nova de Gaia'),
+(1500,'Amadora'),
+(4700,'Braga'),
+(9000,'Funchal'),
+(3030,'Coimbra'),
+(2900,'Setúbal'),
+(2735,'Alguava-Cacém'),
+(2605,'Queluz'),
+(4800,'Guimarãe'),
+(4436,'Rio Tinto'),
+(2830,'Barreiro'),
+(2400,'Leiria'),
+(1675,'Odivelas'),
+(3500,'Viseu'),
+(3800,'Aveiro');
+
+select *from cidade;
